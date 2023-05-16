3ae759dd60d6c9b5c4f9b2e64ca40c8c08a9ce7a
Samuel de Lucas <samuel.lucas@estudiante.uam.es>
2023-05-16 16:53:08 +0200

    Added pin selection screen. The text shown there will be printed prettier in the final version

--- kahoot/kahoot.ino
+++ kahoot/kahoot.ino
@@ -18,6 +18,9 @@
 #define KH_YELLOW_OFF RGB565(95, 62, 1)
 #define KH_GREEN_OFF RGB565(15, 51, 4)
 
+#define UP_ARROW 0x1E
+#define DOWN_ARROW 0x1F
+
 struct coord {
    int x;
    int y;
@@ -25,8 +28,6 @@ struct coord {
 
 class ButtonManager {
     public:
-        ButtonManager() {}
-
         short getSelection(){
             return 2*selection[0] + selection[1];
         }
@@ -54,6 +55,7 @@ class ButtonManager {
             for (int i=0; i<4; i++){
                 drawButton(i, i == answer, true);
             }
+            // TODO: send answer to server
         }
 
     private:
@@ -105,8 +107,68 @@ void answer_selection_loop(){
     buttonManager.selectAnswer(answer);
 }
 
+void pin_selection(){
+    short digits[3] = {0,0,0};
+    const short positions[3] = {SCREEN_WIDTH/2 -20-5-10, SCREEN_WIDTH/2-10, SCREEN_WIDTH/2 +10+5};
+    short pin = 0;
+    short i = 0;
+
+    gfx->fillScreen(BLACK);
+    gfx->setTextColor(WHITE);
+    gfx->setTextSize(1);
+    gfx->setCursor(0, 0);
+    gfx->println("Introduce el pin de la pantalla. Pulsa A para continuar"); // TODO: print prettier
+    bool changed = true;
+
+    do {
+        buttons_loop();
+        
+        if(buttonUp.isPressed()){
+            digits[i] = (digits[i] + 1) % 10;
+            changed = true;
+        } else if (buttonDown.isPressed()){
+            digits[i] = (digits[i] +9) % 10;
+            changed = true;
+        } else if (buttonRight.isPressed()){
+            i = (i + 1) % 3;
+            changed = true;
+        } else if (buttonLeft.isPressed()){
+            i = (i + 2) % 3;
+            changed = true;
+        }
+
+        if (changed){
+            gfx->fillRect(positions[i], SCREEN_HEIGHT/2 - 14, 20, 28, BLACK);
+            for (int j=0; j<3; j++){
+                gfx->setTextSize(2);
+                gfx->drawChar(positions[j]+5, SCREEN_HEIGHT/2 - 14 - 30+7, UP_ARROW, j==i ? LIGHTGREY : BLACK, BLACK);
+                gfx->drawChar(positions[j]+5, SCREEN_HEIGHT/2 - 14 + 30+7, DOWN_ARROW, j==i ? LIGHTGREY : BLACK, BLACK);
+
+                gfx->setTextSize(4);
+                gfx->setTextColor(j==i ? WHITE : DARKGREY);
+                gfx->setCursor(positions[j], SCREEN_HEIGHT/2 - 14);
+                gfx->print(digits[j]);
+                gfx->setTextColor(j==i ? WHITE : BLACK);
+            }
+            
+        }        
+
+        if(buttonA.isPressed()){
+            pin = digits[0]*100 + digits[1]*10 + digits[2];
+        }
+        changed = false;
+    } while (!pin);
+
+    // TODO: send pin to server
+    // TODO: dont exit the while loop if the pin is wrong
+
+    gfx->fillScreen(BLACK);
+}
+
 void setup(){
     setup_badge();
+    pin_selection();
+    // TODO: waiting screen
     answer_selection_loop();
 }
 
